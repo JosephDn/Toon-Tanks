6d67a7a909011c313ddf90de1cd9efe8a959d4ee
Joseph Dorn <joey.dorn7@gmail.com>
2021-03-29 18:12:18 +0100

    Destroy player if dead

--- Source/ToonTanks/Pawns/PawnTank.h
+++ Source/ToonTanks/Pawns/PawnTank.h
@@ -29,6 +29,7 @@ private:
 	float RotateSpeed = 100.f;
 
 	APlayerController* PlayerControllerRef;
+	bool bIsPlayerAlive = true;
 
 	void CalculateMoveInput(float Value);
 	void CalculateRotateInput(float Value);
@@ -47,9 +48,9 @@ public:
 
 	virtual void HandleDestruction() override;
 
+	bool GetIsPlayerAlive();
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-
 };
